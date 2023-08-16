a724a554e866bfa933035e95749ef25c7bde430e
Cameron Candau <camcandau@gmail.com>
2023-08-15 21:06:58 -0700

    Cleaned and commented code.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from ocr import extractText
 from PIL import Image
 import time
 import pyperclip
+import pytesseract
 import os
 
+# Uses Tesseract (pytesseract) OCR to extract text from image and return without leading/trailing whitespace.
+def extractText(imagePath):
+    text = (pytesseract.image_to_string(Image.open(imagePath), lang='jpn+eng'))
+    return text.strip()
+
+# Returns true if file at `image_path` is a valid image, false otherwise.
 def is_valid_image(image_path):
     if image_path.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
         try:
@@ -14,24 +20,27 @@ def is_valid_image(image_path):
             return False
 
 def main():
+    # TODO: Make `source_folder` into command line argument.
+    # IMPORTANT: Use a dedicated folder for `source_folder`!
+    # Program will OCR and delete any images in `source_folder` that contain text.
     source_folder = "~/.quickshot_temp/"
+
     source_folder = os.path.expanduser(source_folder)
     print(f"Watching: {source_folder}")
     while True:
+        # List filenames in source_folder.
         files = os.listdir(source_folder) 
         for file in files:
             full_path = os.path.join(source_folder, file)
-            
             if is_valid_image(full_path):
-                image = Image.open(full_path)
                 text = extractText(full_path)
                 if text != "":
                     print(f"{file} contains text: \n{text}")
                     pyperclip.copy(text)
                     print("Text copied to clipboard")
                     os.remove(full_path)
-                    print("File deleted")
-        time.sleep(0.5)
+                    print("Temp image deleted: ", full_path)
+        time.sleep(0.1)
 
 if __name__ == "__main__":
     main()
--- ocr.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-import pytesseract
-
-def extractText(imagePath):
-    text = (pytesseract.image_to_string(Image.open(imagePath), lang='jpn+eng'))
-    return text.strip()
