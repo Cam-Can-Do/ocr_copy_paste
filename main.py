a44a35b8777f85765adb319bb16cccabb962bec3
Cameron Candau <camcandau@gmail.com>
2023-08-18 07:59:16 -0700

    Moved code from unnessary main function

--- main.py
+++ main.py
@@ -19,28 +19,24 @@ def is_valid_image(image_path):
         except (IOError, SyntaxError):
             return False
 
-def main():
-    # TODO: Make `source_folder` into command line argument.
-    # IMPORTANT: Use a dedicated folder for `source_folder`!
-    # Program will OCR and delete any images in `source_folder` that contain text.
-    source_folder = "~/.quickshot_temp/"
+# TODO: Make `source_folder` into command line argument.
+# IMPORTANT: Use a dedicated folder for `source_folder`!
+# Program will OCR and delete any images in `source_folder` that contain text.
+source_folder = "~/.quickshot_temp/"
 
-    source_folder = os.path.expanduser(source_folder)
-    print(f"Watching: {source_folder}")
-    while True:
-        # List filenames in source_folder.
-        files = os.listdir(source_folder) 
-        for file in files:
-            full_path = os.path.join(source_folder, file)
-            if is_valid_image(full_path):
-                text = extractText(full_path)
-                if text != "":
-                    print(f"{file} contains text: \n{text}")
-                    pyperclip.copy(text)
-                    print("Text copied to clipboard")
-                    os.remove(full_path)
-                    print("Temp image deleted: ", full_path)
-        time.sleep(0.1)
-
-if __name__ == "__main__":
-    main()
+source_folder = os.path.expanduser(source_folder)
+print(f"Watching: {source_folder}")
+while True:
+    # List filenames in source_folder.
+    files = os.listdir(source_folder) 
+    for file in files:
+        full_path = os.path.join(source_folder, file)
+        if is_valid_image(full_path):
+            text = extractText(full_path)
+            if text != "":
+                print(f"{file} contains text: \n{text}")
+                pyperclip.copy(text)
+                print("Text copied to clipboard")
+                os.remove(full_path)
+                print("Temp image deleted: ", full_path)
+    time.sleep(0.1)
